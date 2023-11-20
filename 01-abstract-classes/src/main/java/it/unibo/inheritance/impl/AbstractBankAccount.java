862975a02841ff7d1a7fb1a128d4085f381d62e8
tomm1341 <tommaso.foca@studio.unibo.it>
2023-11-20 10:13:57 +0100

    Added fields to the class and created costructor

--- 01-abstract-classes/src/main/java/it/unibo/inheritance/impl/AbstractBankAccount.java
+++ 01-abstract-classes/src/main/java/it/unibo/inheritance/impl/AbstractBankAccount.java
@@ -1,7 +1,22 @@
 package it.unibo.inheritance.impl;
 
+import it.unibo.inheritance.api.AccountHolder;
 import it.unibo.inheritance.api.BankAccount;
 
 public abstract class AbstractBankAccount implements BankAccount{
     
+    private static final double ATM_TRANSACTION_FEE = 1;
+    private static final double MANAGEMENT_FEE = 5;
+
+    private final AccountHolder holder;
+    private double balance;
+    private int transactions;
+
+
+
+    public AbstractBankAccount(final AccountHolder accountHolder, final double balance) {
+        this.holder = accountHolder;
+        this.balance = balance;
+        this.transactions = 0;
+    }
 }
